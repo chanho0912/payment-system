55aae8bfecf743fd8dcc1d49a7657fc13e172468
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-06-18 23:14:05 +0900

    Test Payment Confirmation

--- src/main/kotlin/com/noah/paymentsystem/payment/application/domain/PaymentMethod.kt
+++ src/main/kotlin/com/noah/paymentsystem/payment/application/domain/PaymentMethod.kt
@@ -1,11 +1,11 @@
 package com.noah.paymentsystem.payment.application.domain
 
-enum class PaymentMethod(description: String) {
-    EASY_PAY("간편 결제");
+enum class PaymentMethod(val method: String) {
+    EASY_PAY("간편결제");
 
     companion object {
         fun from(method: String): PaymentMethod {
-            return entries.find { it.name == method } ?: error("결제 방식 (method: $method) 는 올바르지 않은 방식입니다.")
+            return entries.find { it.method == method } ?: error("결제 방식 (method: $method) 는 올바르지 않은 방식입니다.")
         }
     }
 }
